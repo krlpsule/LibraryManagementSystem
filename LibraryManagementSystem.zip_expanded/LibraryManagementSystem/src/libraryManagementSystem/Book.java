daf7e4fd9e12a53d7da81462e50ce3e2e94b4c48
Mertvyy <137713445+Mertvyy@users.noreply.github.com>
2025-12-30 17:22:27 +0300

    Update Book.java
    with comments adn tc

--- LibraryManagementSystem.zip_expanded/LibraryManagementSystem/src/libraryManagementSystem/Book.java
+++ LibraryManagementSystem.zip_expanded/LibraryManagementSystem/src/libraryManagementSystem/Book.java
@@ -1,31 +1,31 @@
 package libraryManagementSystem;
 
-public class Book {
+public class Book {//book class has id, name, author and availability bool
     private int id;
     private String title;
     private String author;
     private boolean isAvailable;
 
-    public Book(int id, String title, String author, boolean isAvailable) {
+    public Book(int id, String title, String author, boolean isAvailable) {//initialization
         this.id = id;
         this.title = title;
         this.author = author;
         this.isAvailable = isAvailable;
     }
 
-    public int getId() { return id; }
+    public int getId() { return id; }//getters
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public boolean isAvailable() { return isAvailable; }
     public void setAvailable(boolean available) { isAvailable = available; }
 
    
-    public String toFileString() {
+    public String toFileString() {//saving to file
         return id + "," + title + "," + author + "," + isAvailable;
     }
 
     @Override
-    public String toString() {
+    public String toString() {//method to write out the insides to the users
         return "ID: " + id + " | Title: " + title + " | Author: " + author + " | Available: " + (isAvailable ? "Yes" : "No");
     }
 }
