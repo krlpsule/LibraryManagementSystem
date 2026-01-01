0be2d7ed47eb661580e74837e5d03c478a44b3a6
ahsndrmz05 <ahsendurmaztr@gmail.com>
2026-01-01 15:18:17 +0300

    Enhance Queue.java with explanatory comments
    Added comments to clarify queue operations and conditions.

--- LibraryManagementSystem.zip_expanded/LibraryManagementSystem/src/libraryManagementSystem/Queue.java
+++ LibraryManagementSystem.zip_expanded/LibraryManagementSystem/src/libraryManagementSystem/Queue.java
@@ -19,25 +19,31 @@ public class Queue<T> {
     private class Node {
         T data;
         Node next;
-        Node(T data) { this.data = data; }
+        //constructor
+        Node(T data) { this.data = data;}
     }
     //Queue have to keep track of front and rear
     private Node front, rear;
     //Worst case time complexity is O(1), because we don't traverse the queue to find rear. 
     public void enqueue(T request) {
         Node n = new Node(request);
-        if (rear == null) front = rear = n;
-        else {
+        //checks if the queue is empty
+        if (rear == null) front = rear = n; //Queue is empty, then just insert new request.
+        else { //Queue is not empty, then insert new request and update rear.
             rear.next = n;
             rear = n;
         }
     }
     //Worst case time complexity is O(1), because we don't traverse the queue to find front. 
     public T dequeue() {
-        if (front == null) return null;
+        //checks if the queue is empty
+        if (front == null) return null; //Empty queue, nothing to delete, returns null
+        //If not empty, temporarly store value of front and delete the front node
         T data = front.data;
         front = front.next;
+        //If there is no other element left, update rear.
         if (front == null) rear = null;
+        //return the temporarly stored value of old front
         return data;
     }
     //Worst case time complexity is O(1), because isEmpty method just checks if the queue has data or not. 
